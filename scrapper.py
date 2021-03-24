3215f4bd93b97c05e381d4e38cd7abf8a089cbfb
Will Funk <jwfunk@the-computer.lan>
2021-03-24 14:21:15 -0500

    Fixed email automation

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import smtplib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,23 +8,26 @@ while True:
 	driver.get("https://www.walgreens.com/findcare/vaccination/covid-19")
 	continue_link = driver.find_element_by_link_text('Schedule new appointment')
 	continue_link.click()
-	time.sleep(7)
-	driver.find_element_by_xpath('//*[@id="wag-body-main-container"]/section/section/section/section/section[2]/div/span/button').click()
 	time.sleep(2)
-	if driver.find_element_by_xpath('//*[@id="wag-body-main-container"]/section/section/section/section/div/a/span[2]/p').text == 'Appointments Available!':
-		print('Yay!')
-		body = 'Vaccine Available'
-		try:
-		    smtpObj = smtplib.SMTP('smtp.outlook.com', 587)
-		except Exception as e:
-		    print(e)
-		    smtpObj = smtplib.SMTP_SSL('smtp-mail.outlook.com', 465)
-		#type(smtpObj) 
-		smtpObj.ehlo()
-		smtpObj.starttls()
-		smtpObj.login('jwfunkbot@outlook.com', "goddEr-fikbic-percu4") 
-		smtpObj.sendmail('jwfunkbot@outlook.com', 'jwfunk@outlook.com', body) # Or recipient@outlook
+	driver.find_element_by_xpath('//*[@id="wag-body-main-container"]/section/section/section/section/section[2]/div/span/button').click()
+	time.sleep(3)
+	try:
+		if driver.find_element_by_xpath('//*[@id="wag-body-main-container"]/section/section/section/section/section[1]/section/div/a/span[2]/p').text == 'Appointments Available!':
+			print('Yay!')
+			body = 'Vaccine Available'
+			try:
+			    smtpObj = smtplib.SMTP('smtp.outlook.com', 587)
+			except Exception as e:
+			    print(e)
+			    smtpObj = smtplib.SMTP_SSL('smtp-mail.outlook.com', 465)
+			#type(smtpObj) 
+			smtpObj.ehlo()
+			smtpObj.starttls()
+			smtpObj.login('sender@outlook.com', "password") 
+			smtpObj.sendmail('sender@outlook.com', 'reciever@outlook.com', body) # Or recipient@outlook
 
-		smtpObj.quit()
+			smtpObj.quit()
+	except Exception as e:
+		print(str(datetime.datetime.now()))
 	driver.quit()
-	time.sleep(60)
+	time.sleep(300)
